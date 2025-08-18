e38b7d8058e88e9caa4840a3e20a7cd419ddf71d
HipocratesDeCos <andresragel2024@gmail.com>
2025-08-18 14:13:20 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,2 @@
 // js/main.js
-
-// Este archivo está vacío porque solo sirve como "punto de entrada" para el HTML.
-// No contiene lógica, solo es necesario para que el HTML cargue los scripts.
+// Este archivo vacío es necesario para que el navegador cargue los scripts
